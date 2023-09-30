8226af53ec74ab8aba7150ce9ee844d17e3b6e2c
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-09-29 21:12:50 -0500

    Add files via upload

---
+++ src/API.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '45925bb012msh22bfb8925b1cc6cp1d203fjsn70298151a737',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b476226f6a011b889e683a2a989c1a41";
